697e7ce7fdde57638488d0c04846ac42a09d69d7
beanieboi <beanie@benle.de>
2013-02-22 02:01:10 +0100

    update postgres-config.cmake

--- postgres-config.cmake
+++ postgres-config.cmake
@@ -1,36 +1,57 @@
-# - Find PostgreSQL library
-2 #
-3 # This module defines:
-4 #  POSTGRESQL_FOUND - True if the package is found
-5 #  POSTGRESQL_INCLUDE_DIR - containing libpq-fe.h
-6 #  POSTGRESQL_LIBRARIES - Libraries to link to use PQ functions.
-7
-8 if (POSTGRESQL_INCLUDE_DIR AND POSTGRESQL_LIBRARIES)
-9   set(POSTGRESQL_FIND_QUIETLY TRUE)
-10  endif (POSTGRESQL_INCLUDE_DIR AND POSTGRESQL_LIBRARIES)
-11
-12  # Include dir
-13  find_path(POSTGRESQL_INCLUDE_DIR
-14          NAMES libpq-fe.h
-15          PATH_SUFFIXES pgsql postgresql
-16  )
-17
-18  # Library
-19  find_library(POSTGRESQL_LIBRARY
-20    NAMES pq
-21  )
-22
-23  # handle the QUIETLY and REQUIRED arguments and set POSTGRESQL_FOUND to TRUE if
-24  # all listed variables are TRUE
-25  INCLUDE(FindPackageHandleStandardArgs)
-26  FIND_PACKAGE_HANDLE_STANDARD_ARGS(POSTGRESQL DEFAULT_MSG POSTGRESQL_LIBRARY POSTGRESQL_INCLUDE_DIR)
-27
-28  IF(POSTGRESQL_FOUND)
-29    SET( POSTGRESQL_LIBRARIES ${POSTGRESQL_LIBRARY} )
-30  ELSE(POSTGRESQL_FOUND)
-31    SET( POSTGRESQL_LIBRARIES )
-32  ENDIF(POSTGRESQL_FOUND)
-33
-34  # Lastly make it so that the POSTGRESQL_LIBRARY and POSTGRESQL_INCLUDE_DIR variables
-35  # only show up under the advanced options in the gui cmake applications.
-36  MARK_AS_ADVANCED( POSTGRESQL_LIBRARY POSTGRESQL_INCLUDE_DIR )
+# - Find PostgreSQL
+# Find the PostgreSQL includes and client library
+# This module defines
+#  POSTGRESQL_INCLUDE_DIR, where to find POSTGRESQL.h
+#  POSTGRESQL_LIBRARIES, the libraries needed to use POSTGRESQL.
+#  POSTGRESQL_FOUND, If false, do not try to use PostgreSQL.
+#
+# Copyright (c) 2006, Jaroslaw Staniek, <js@iidea.pl>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+# Add the postgresql and mysql include paths here
+
+if(POSTGRESQL_INCLUDE_DIR AND POSTGRESQL_LIBRARIES)
+   set(POSTGRESQL_FOUND TRUE)
+
+else(POSTGRESQL_INCLUDE_DIR AND POSTGRESQL_LIBRARIES)
+
+#  find_path(POSTGRESQL_INCLUDE_DIR libpq-fe.h
+
+ FIND_PATH(POSTGRESQL_INCLUDE_DIR postgres.h
+      /usr/include/server
+      /usr/include/pgsql/server
+      /usr/local/include/pgsql/server
+      /usr/include/postgresql/server
+      /usr/include/postgresql/*/server
+      /usr/local/include/postgresql/server
+      /usr/local/include/postgresql/*/server
+      $ENV{ProgramFiles}/PostgreSQL/*/include/server
+      $ENV{SystemDrive}/PostgreSQL/*/include/server
+      )
+
+  find_library(POSTGRESQL_LIBRARIES NAMES pq libpq
+     PATHS
+     /usr/lib
+     /usr/local/lib
+     /usr/lib/postgresql
+     /usr/lib64
+     /usr/local/lib64
+     /usr/lib64/postgresql
+     $ENV{ProgramFiles}/PostgreSQL/*/lib/ms
+     $ENV{SystemDrive}/PostgreSQL/*/lib/ms
+     )
+
+  if(POSTGRESQL_INCLUDE_DIR AND POSTGRESQL_LIBRARIES)
+    set(POSTGRESQL_FOUND TRUE)
+    message(STATUS "Found PostgreSQL: ${POSTGRESQL_INCLUDE_DIR}, ${POSTGRESQL_LIBRARIES}")
+    INCLUDE_DIRECTORIES(${POSTGRESQL_INCLUDE_DIR})
+  else(POSTGRESQL_INCLUDE_DIR AND POSTGRESQL_LIBRARIES)
+    set(POSTGRESQL_FOUND FALSE)
+    message(STATUS "PostgreSQL not found.")
+  endif(POSTGRESQL_INCLUDE_DIR AND POSTGRESQL_LIBRARIES)
+
+  mark_as_advanced(POSTGRESQL_INCLUDE_DIR POSTGRESQL_LIBRARIES)
+
+endif(POSTGRESQL_INCLUDE_DIR AND POSTGRESQL_LIBRARIES)
